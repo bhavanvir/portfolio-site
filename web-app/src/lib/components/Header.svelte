11823375bd29ee57991d970ac986d35eb2ced391
bhavanvir <bhavanvir.r@gmail.com>
2024-01-13 22:44:07 -0800

    Added background to header when scrolling

--- web-app/src/lib/components/Header.svelte
+++ web-app/src/lib/components/Header.svelte
@@ -2,6 +2,8 @@
   import LogoIcon from "$lib/assets/icons/LogoIcon.svg?raw";
   import ArrowNarrowRightIcon from "$lib/assets/icons/ArrowNarrowRightIcon.svg?raw";
 
+  let y = 0;
+
   let context = "menu";
   function onClick() {
     const mobileMenu = document.querySelector(".mobile-menu");
@@ -11,14 +13,22 @@
   }
 </script>
 
-<nav class="sticky top-0">
+<nav
+  class={y > 50
+    ? "sticky top-0 bg-secondary-background text-secondary-text transition-all ease-in delay-100"
+    : "sticky top-0 transition-all ease-out delay-100"}
+>
   <div class="max-w-[100rem] mx-auto">
     <div class="flex justify-between font-uncut">
       <div class="flex space-x-4">
         <!-- Logo -->
         <div>
           <a href="/" class="flex items-center py-4 px-2">
-            <svg class="h-10 w-10 mr-2">
+            <svg
+              class={y > 50
+                ? "h-10 w-10 mr-2 filter-white transition-all ease-in delay-100"
+                : "h-10 w-10 mr-2 transition-all ease-out delay-100"}
+            >
               {@html LogoIcon}
             </svg>
             <span class="font-bold text-2xl">bhavanvir</span>
@@ -115,3 +125,13 @@
     <div class="flex-grow border-t-2" />
   </div>
 </nav>
+
+<svelte:window bind:scrollY={y} />
+
+<style>
+  /* Filter colour found using: https://codepen.io/sosuke/pen/Pjoqqp*/
+  .filter-white {
+    filter: invert(100%) sepia(100%) saturate(0%) hue-rotate(322deg)
+      brightness(101%) contrast(102%);
+  }
+</style>
