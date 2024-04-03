9ad95954b0defc4f2d36e326bf9c49a7d1024e35
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-04-02 23:59:06 -0700

    Remove text justification from Hero section

--- web-app/src/lib/components/Hero/Hero.svelte
+++ web-app/src/lib/components/Hero/Hero.svelte
@@ -1,6 +1,6 @@
 <div class="max-w-[100rem] px-2 mx-auto">
   <div class="hero-spacing flex items-center">
-    <p class="font-uncut text-4xl sm:text-6xl text-justify">
+    <p class="font-uncut text-4xl sm:text-6xl">
       <span class="underline decoration-wavy decoration-red-500">Bhavanvir</span
       > is a Software Engineer based in British Columbia that designs scalable and
       efficient solutions to complex problems, with an emphasis on human-centered
