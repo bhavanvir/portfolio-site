4df17b1d7fdcc35dd5b96df9a8e24c08de194d31
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-05-30 10:58:00 -0700

    Add stars from github repos to page

--- web-app/src/lib/components/Work/Work.svelte
+++ web-app/src/lib/components/Work/Work.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import data from "$lib/components/Work/projects.json";
+  import { Star } from "svelte-lucide";
 
   const tagColors: Record<string, string> = {
     Completed: "bg-green-100 text-green-800",
@@ -17,19 +18,28 @@
     <div class="grid md:grid-cols-2 xl:grid-cols-2 gap-6">
       {#each data as project}
         <div class="border-2 p-6">
-          <a
-            href={project.github}
-            target="_blank"
-            class="text-lg font-bold hover:underline decoration-2"
-          >
-            {project.title}
-          </a>
-          {#each project.tags ?? [] as tag}
-            <span
-              class={`inline-block text-xs font-medium me-2 px-2.5 py-0.5 border-2 ${tagColors[tag]}`}
-              >{tag}</span
-            >
-          {/each}
+          <div class="flex justify-between items-center">
+            <div class="flex justify-start space-x-1 items-center">
+              <a
+                href={project.github}
+                target="_blank"
+                class="text-lg font-bold hover:underline decoration-2"
+              >
+                {project.title}
+              </a>
+              {#each project.tags ?? [] as tag}
+                <span
+                  class={`inline-block text-xs font-medium me-2 px-2.5 py-0.5 border-2 ${tagColors[tag]}`}
+                  >{tag}</span
+                >
+              {/each}
+            </div>
+
+            <span class="flex items-center">
+              <Star class="inline-block mr-1 fill-yellow-500 h-4 w-4" />
+              {project.stars}
+            </span>
+          </div>
 
           <p class="text-md text-slate-700">{project.date}</p>
           {#if project.description}
