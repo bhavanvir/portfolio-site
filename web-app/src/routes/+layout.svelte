a9bbe8a946bc93292715857487717e8d0ddbddc8
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-05-30 09:00:33 -0700

    Add vercel analytics

--- web-app/src/routes/+layout.svelte
+++ web-app/src/routes/+layout.svelte
@@ -1,7 +1,14 @@
+<script context="module">
+  import { dev } from "$app/environment";
+</script>
+
 <script>
+  import { inject } from "@vercel/analytics";
   import Header from "$lib/components/Header/Header.svelte";
   import Footer from "$lib/components/Footer/Footer.svelte";
   import "../app.css";
+
+  inject({ mode: dev ? "development" : "production" });
 </script>
 
 <svelte:head>
