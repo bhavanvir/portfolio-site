c17e83625961f98e3a7a9483cc66e7545dec2981
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-07-03 22:37:29 -0700

    Fix styling for footer

--- web-app/src/lib/components/Footer.svelte
+++ web-app/src/lib/components/Footer.svelte
@@ -2,17 +2,17 @@
   import { ChevronLeft, ChevronRight } from "svelte-lucide";
 </script>
 
-<div class="w-full mx-auto flex items-center justify-center">
+<div class="flex items-center justify-center py-4">
   <div class="flex justify-between w-full max-w-lg">
     <span
       class="rounded-full glass-effect p-2 flex items-center justify-center"
     >
-      <ChevronLeft class="h-6 w-6 stroke-primary-foreground" />
+      <ChevronLeft class="h-6 w-6 text-primary-foreground" />
     </span>
     <span
       class="rounded-full glass-effect p-2 flex items-center justify-center"
     >
-      <ChevronRight class="h-6 w-6 stroke-primary-foreground" />
+      <ChevronRight class="h-6 w-6 text-primary-foreground" />
     </span>
   </div>
 </div>
