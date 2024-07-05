09ce0bc51ccf4582b0b4b30dedc556f62c63ef09
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-07-04 18:57:26 -0700

    Add another link to navbar and remove glass-effect

--- web-app/src/lib/components/NavBar.svelte
+++ web-app/src/lib/components/NavBar.svelte
@@ -2,21 +2,15 @@
   <div
     class="w-full max-w-lg p-2 flex justify-center glass-effect rounded-full"
   >
-    <span class="rounded-full border-none h-6 text-white p-2 flex items-center"
+    <span class="rounded-full border-none h-6 p-2 flex items-center text-lg"
       >About</span
     >
-    <span class="rounded-full border-none h-6 text-white p-2 flex items-center"
+    <span class="rounded-full border-none h-6 p-2 flex items-center text-lg"
       >Projects</span
+    >
+    <span class="rounded-full border-none h-6 p-2 flex items-center text-lg"
+      >Contact</span
     >
   </div>
 </div>
 
-<style lang="css">
-  .glass-effect {
-    background: rgba(255, 255, 255, 0.2);
-    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-    backdrop-filter: blur(5px);
-    -webkit-backdrop-filter: blur(5px);
-    border: 1px solid rgba(255, 255, 255, 0.3);
-  }
-</style>
