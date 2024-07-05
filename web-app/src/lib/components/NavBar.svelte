9f0cbfcd368d4659f9a129189b5a1480ab39fdcf
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-07-04 22:42:21 -0700

    Now displaying the current slide

--- web-app/src/lib/components/NavBar.svelte
+++ web-app/src/lib/components/NavBar.svelte
@@ -1,16 +1,41 @@
+<script>
+  let currentSlide = "About";
+
+  function changeSlide(slide) {
+    currentSlide = slide;
+  }
+</script>
+
 <div class="w-full flex justify-center p-4">
-  <div
-    class="w-full max-w-lg p-2 flex justify-center glass-effect rounded-full"
-  >
-    <span class="rounded-full border-none h-6 p-2 flex items-center text-lg"
-      >About</span
+  <div class="w-full max-w-lg p-2 flex justify-center glass-effect rounded-full">
+    <span
+      class="rounded-full border-none h-6 p-2 flex items-center text-lg cursor-pointer"
+      class:active={currentSlide === "About"}
+      on:click={() => changeSlide("About")}
     >
-    <span class="rounded-full border-none h-6 p-2 flex items-center text-lg"
-      >Projects</span
+      About
+    </span>
+    <span
+      class="rounded-full border-none h-6 p-2 flex items-center text-lg cursor-pointer"
+      class:active={currentSlide === "Projects"}
+      on:click={() => changeSlide("Projects")}
     >
-    <span class="rounded-full border-none h-6 p-2 flex items-center text-lg"
-      >Contact</span
+      Projects
+    </span>
+    <span
+      class="rounded-full border-none h-6 p-2 flex items-center text-lg cursor-pointer"
+      class:active={currentSlide === "Contact"}
+      on:click={() => changeSlide("Contact")}
     >
+      Contact
+    </span>
   </div>
 </div>
+
+<style>
+  .active {
+    text-decoration: underline;
+    text-decoration-thickness: 2px;
+  }
+</style>
 
