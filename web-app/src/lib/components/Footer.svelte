f3618492d14797b0260bd9715ae81540ea7205bb
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-07-06 16:49:59 -0700

    Add logic for cycling between slides

--- web-app/src/lib/components/Footer.svelte
+++ web-app/src/lib/components/Footer.svelte
@@ -1,16 +1,26 @@
 <script>
+  import { currentSlide, setCurrentSlide } from '$lib/store.js';
   import { ChevronLeft, ChevronRight } from "svelte-lucide";
+
+  let slides = ["About", "Projects", "Contact"];
+  function cycleSlides(direction) {
+    let currentSlideIndex = slides.indexOf($currentSlide);
+    let nextSlideIndex = (currentSlideIndex + direction + slides.length) % slides.length;
+    setCurrentSlide(slides[nextSlideIndex]);
+  }
 </script>
 
 <div class="flex items-center justify-center py-4">
   <div class="flex justify-center space-x-2 w-full max-w-lg">
     <span
       class="rounded-full glass-effect p-2 flex items-center justify-center"
+      on:click={() => cycleSlides(-1)}
     >
       <ChevronLeft class="h-6 w-6 text-primary-foreground" />
     </span>
     <span
       class="rounded-full glass-effect p-2 flex items-center justify-center"
+      on:click={() => cycleSlides(1)}
     >
       <ChevronRight class="h-6 w-6 text-primary-foreground" />
     </span>
