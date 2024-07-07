a041604d39d43db673ceffe126894afbf19ce2e3
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-07-06 20:21:52 -0700

    Add Contact page

---
+++ web-app/src/lib/components/Contact.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { ArrowUpRight } from "svelte-lucide";
+</script>
+
+<div class="max-w-2xl">
+  <h1 class="text-4xl text-center">
+    Have a question, proposal, or just want to chat? Feel free to reach out to me at any of the platforms I'm active on 
+  </h1>
+
+  <div class="max-w-sm mx-auto pt-4">
+    <div class="grid grid-cols-[20%_80%] text-xl gap-2">
+      Email
+      <span class="flex items-center justify-end hover:underline decoration-2">
+        <a href="mailto:me@bhavanvir.ca">me@bhavanvir.ca</a>
+        <ArrowUpRight class="ml-2" />
+      </span>
+
+      LinkedIn
+      <span class="flex items-center justify-end hover:underline decoration-2">
+        <a href="https://www.linkedin.com/in/bhavanvir/">bhavanvir</a>
+        <ArrowUpRight class="ml-2" />
+      </span>
+
+      GitHub
+      <span class="flex items-center justify-end hover:underline decoration-2">
+        <a href="https://www.github.com/bhavanvir">bhavanvir</a>
+        <ArrowUpRight class="ml-2" />
+      </span>
+    </div>
+  </div>
+</div>
