6689d6ba1352b9332c63c7de8be754fe90fa70c2
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-07-06 20:22:19 -0700

    Add contact page link to landing and add padding

--- web-app/src/routes/+page.svelte
+++ web-app/src/routes/+page.svelte
@@ -4,6 +4,7 @@
   import NavBar from "$lib/components/NavBar.svelte";
   import Footer from "$lib/components/Footer.svelte";
   import About from "$lib/components/About.svelte";
+  import Contact from "$lib/components/Contact.svelte";
 </script>
 
 <svelte:head>
@@ -16,11 +17,13 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 </svelte:head>
 
-<main class="min-h-screen flex flex-col">
+<main class="min-h-screen flex flex-col px-8">
   <NavBar />
   <div class="flex-1 flex items-center justify-center mx-auto">
     {#if $currentSlide === "About"}
       <About />
+    {:else if $currentSlide === "Contact"}
+      <Contact />
     {/if}
   </div>
   <Footer />
