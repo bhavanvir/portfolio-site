5bc0dd981b75984d7b443cb3400287198eb866f1
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-07-16 21:31:47 -0700

    Remove status from contact page and re-order divs to better align with other components

--- web-app/src/lib/components/Contact.svelte
+++ web-app/src/lib/components/Contact.svelte
@@ -1,60 +1,9 @@
 <script>
-  import { onMount } from "svelte";
-  import {
-    ArrowUpRight,
-    ArrowRight,
-    Bell,
-    BellRing,
-    BellOff,
-  } from "svelte-lucide";
-
-  let status = "";
-  let currentMessage = "";
-
-  const updateMessageAndColor = () => {
-    const now = new Date();
-    const day = now.getDay();
-    const hour = now.getHours();
-    if (day === 0 || day === 6) {
-      // Weekend
-      currentMessage = "I'm unlikely to respond on weekends";
-      status = "Offline";
-    } else if (hour < 9 || hour >= 17) {
-      // Weekdays after 5 PM or before 9 AM
-      currentMessage = "I may be slower to respond outside of work hours";
-      status = "Away";
-    } else {
-      // Weekdays between 9 AM and 5 PM
-      currentMessage = "I should be able to respond promptly!";
-      status = "Online";
-    }
-  };
-
-  onMount(() => {
-    updateMessageAndColor();
-
-    const interval = setInterval(updateMessageAndColor, 60000);
-    return () => clearInterval(interval);
-  });
+  import { ArrowUpRight, ArrowRight } from "svelte-lucide";
 </script>
 
 <div class="max-w-4xl">
-  <div class="pb-4 flex justify-center items-center">
-    {#if status === "Offline"}
-      <BellOff class="mr-2" />
-    {:else if status === "Away"}
-      <BellRing class="mr-2" />
-    {:else if status === "Online"}
-      <Bell class="mr-2" />
-    {/if}
-    <span class="text-lg md:text-xl">{currentMessage}</span>
-  </div>
-
-  <h1 class="text-2xl md:text-4xl text-center">
-    Have a question, proposal, or just want to chat? Reach out!
-  </h1>
-
-  <div class="flex pt-4 text-lg md:text-xl gap-2 space-x-4 justify-center">
+  <div class="flex pb-4 text-lg md:text-xl gap-2 space-x-4 justify-center">
     <span class="flex items-center justify-end hover:underline decoration-2">
       <a href="mailto:me@bhavanvir.ca">Email</a>
       <ArrowRight class="ml-2" />
@@ -72,4 +21,8 @@
       <ArrowUpRight class="ml-2" />
     </span>
   </div>
+
+  <h1 class="text-2xl md:text-4xl text-center">
+    Have a question, proposal, or just want to chat? Feel free to reach out
+  </h1>
 </div>
