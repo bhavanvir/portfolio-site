bc1268e2883b6fcf9a112fcd2ee51656fe4ca96f
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-08-09 23:58:03 -0700

    Change arrow direction for email link

--- web-app/src/lib/components/Contact.svelte
+++ web-app/src/lib/components/Contact.svelte
@@ -1,12 +1,12 @@
 <script>
-  import { ArrowUpRight, ArrowRight } from "svelte-lucide";
+  import { ArrowUpRight } from "svelte-lucide";
 </script>
 
 <div class="max-w-4xl">
   <div class="flex pb-4 text-lg md:text-xl gap-2 space-x-4 justify-center">
     <span class="flex items-center justify-end hover:underline decoration-2">
       <a href="mailto:me@bhavanvir.ca">Email</a>
-      <ArrowRight class="ml-2" />
+      <ArrowUpRight class="ml-2" />
     </span>
 
     <span class="flex items-center justify-end hover:underline decoration-2">
