1481ec9edcd2f9c53cf20a5726985260247a9e27
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-12-20 12:28:03 -0800

    Remove Svelte project in favour for Next.js

--- web-app/src/lib/components/ui/card/card-description.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import { cn } from "$lib/utils.js";
-  let className = undefined;
-  export { className as class };
-</script>
-
-<p class={cn("text-sm text-primary-foreground/50", className)} {...$$restProps}>
-  <slot />
-</p>
--- web-app/src/lib/store.js
+++
@@ -1,7 +0,0 @@
-import { writable } from "svelte/store";
-
-export let currentSlide = writable("About");
-
-export function setCurrentSlide(slide) {
-  currentSlide.set(slide);
-}
