1c04ac776612a2cf17b5b47fb821d9ca4b4bc948
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-12-22 13:42:00 -0800

    Fix typo

--- portfolio-site/app/resume/page.tsx
+++ portfolio-site/app/resume/page.tsx
@@ -4,7 +4,7 @@ export const dynamic = "force-dynamic";
 
 export const metadata = {
   title: "Resume",
-  description: "Look at resume.",
+  description: "Look at my resume.",
 };
 
 export default function Resume() {
