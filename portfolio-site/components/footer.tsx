382be4211903a7159cafb6b9e109293361257d02
Bhavanvir Rai <bhavanvir.r@gmail.com>
2025-01-31 14:53:24 -0800

    Fix class names

--- portfolio-site/components/footer.tsx
+++ portfolio-site/components/footer.tsx
@@ -9,9 +9,9 @@ export default function Footer() {
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           className="h-4 w-4 mr-1"
         >
           <circle cx="12" cy="12" r="10" />
